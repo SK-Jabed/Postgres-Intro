47460348e3b73c079c3449b10f75cb752daf0018
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-30 23:35:02 +0600

    New Test view table and data added and managed

--- index.sql
+++ index.sql
@@ -3,12 +3,6 @@
 SELECT * from  employees;
 
 
-
-
-
-
-
-
 EXPLAIN ANALYSE
 SELECT * from employees WHERE emp_no = '10004';
 
--- trigger.sql
+++ trigger.sql
@@ -16,8 +16,6 @@ A trigger is a database object in PostgreSQL (and other database management syst
 -- EXECUTE FUNCTION function_name();
 
 
-
-
 CREATE Table my_users
 (
     user_name VARCHAR(50),
